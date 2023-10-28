7a9ab01d348f6a35a1eba58854a5c26dc39890ec
VyGiang <you@example.com>
2023-10-29 01:03:44 +0300

    add new emoji

--- src/index.js
+++ src/index.js
@@ -1,53 +1,95 @@
-import React from "react";
-import  ReactDOM  from "react-dom/client";
-import"./styles.css";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./styles.css"
 
-
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+]
 function App() {
-    return (
-      <div className="card">
-        <Avatar />
-        <div className="data">
-          <Intro />
-          <SkillList />
-        </div>
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
       </div>
-    );
-  }
+    </div>
+  )
+}
 
-function Avatar(){
-    return <img  className="avatar" src="avatar.jpg" alt="image" />;
+function Avatar() {
+  return <img className="avatar" src="avatar.jpg" alt="image" />
 }
 
 function Intro() {
-    return (
-      <div>
-        <h1 >Vy Giang</h1>
-        <p >Full-stack web developer and teacher at Udemy. When not coding or preparing a course, I like to play board games, to cook (and eat), or to just enjoy the Portuguese sun at the beach.</p>
-      </div>
-    );
-  }
-
-function SkillList(){
-    return (
-        <div className="skill-list">
-            <Skill skill="HTML+CSS" emoji="💪" color="blue" />
-            <Skill skill="React" emoji="💪" color="red" />
-            <Skill skill="JavaScript" emoji="💪" color="yellow" />
-            <Skill skill="Django" emoji="💪" color="pink" />
-        </div>
-    );
-}
-function Skill(progs){
-    console.log(progs)
-    return (
-       <div className="skill" style={{backgroundColor: progs.color }} >
-            <span>{progs.skill}</span>
-            <span>{progs.emoji}</span>
-       </div> 
-    );
+  return (
+    <div>
+      <h1>Vy Giang</h1>
+      <p>
+        Full-stack web developer and teacher at Udemy. When not coding or
+        preparing a course, I like to play board games, to cook (and eat), or to
+        just enjoy the Portuguese sun at the beach.
+      </p>
+    </div>
+  )
 }
 
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill
+          key={skill.skill}
+          skill={skill.skill}
+          level={skill.level}
+          color={skill.color}
+        />
+      ))}
+    </div>
+  )
+}
+function Skill({ skill, color, level }) {
+  console.log(skill)
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "💪"}
+      </span>
+    </div>
+  )
+}
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<App />)
